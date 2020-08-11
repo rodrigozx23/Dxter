1fdc2d7b52ccfbf5d7be6322f039cf8c92abdef1
rodrigo <rodrigzx23@gmail.com>
2020-08-11 00:00:48 -0500

    ELIMINAR_CANTIDADES_PEDIDO_VENTA

--- SP_PEDIDO_VENTA.sql
+++ SP_PEDIDO_VENTA.sql
@@ -6,15 +6,17 @@ CREATE PROCEDURE SP_PEDIDO_VENTA (
 ) 
 BEGIN	
 	/*VARIABLES*/
+    DECLARE finished2 INTEGER DEFAULT 0;
 	DECLARE finished INTEGER DEFAULT 0;
 	DECLARE id int DEFAULT 0;
-    DECLARE id_UNIDADMEDIDA int DEFAULT 0;
+    DECLARE PRO_UNIDADMEDIDA int DEFAULT 0;
     DECLARE ID_ITEM int DEFAULT 0; 
     DECLARE CANTIDAD_ITEM int DEFAULT 0;
     DECLARE PESO_ITEM int DEFAULT 0;
+	DECLARE ITEM_UMEDIDA int DEFAULT 0;   
     
-     DECLARE VALOR_PRODUCTO_DETALLE float DEFAULT 0.00;
-     
+    DECLARE VALOR_PRODUCTO_DETALLE float DEFAULT 0.00;
+    DECLARE CANTIDAD_PEDIDO int DEFAULT 0; 
 	/* SACAR LOS PRODUCTOS*/
     DEClARE curProductoPedido 
 	
@@ -32,45 +34,51 @@ BEGIN
 		IF finished = 1 THEN 
 			LEAVE getIdProducto;
 		END IF;
-		-- caractersticas del producto detalle
-		select id_UNIDADMEDIDA = prod_int_tipo_medida_salida,
-		VALOR_PRODUCTO_DETALLE = prod_dbl_cantidad_item,
-		ID_ITEM = invi_int_id
-		from tb_productos_detalle 
-		where pro_int_id = id;
-		-- caractersticas del ITEM
-		select 
-		CANTIDAD_ITEM = invi_dbl_cantidad_total_item, 
-		PESO_ITEM = invi_dbl_peso_neto
-		from tb_item_inventario
-		where invi_int_id = ID_ITEM;
-		
-		CASE
-					WHEN id_UNIDADMEDIDA = 4 
-                    THEN  
-						-- VALIDACION. que no nea mayor y que exista.
-						UPDATE tb_item_inventario set 
-						  invi_dbl_cantidad_total_item = CANTIDAD_ITEM - VALOR_PRODUCTO_DETALLE
-						  -- sacar id de inventario
-						where invi_int_id = ID_ITEM;                    
-					WHEN id_UNIDADMEDIDA = 3 
-                    THEN -- VALIDACION. que no nea mayor y que exista.
-						UPDATE tb_item_inventario set 
-						  invi_dbl_cantidad_total_item = CANTIDAD_ITEM - VALOR_PRODUCTO_DETALLE
-						  -- sacar id de inventario
-						where invi_int_id = ID_ITEM;   
-                    WHEN id_UNIDADMEDIDA = 2
-                    THEN -- VALIDACION. que no nea mayor y que exista.
-						UPDATE tb_item_inventario set 
-						  invi_dbl_cantidad_total_item = CANTIDAD_ITEM - VALOR_PRODUCTO_DETALLE
-						  -- sacar id de inventario
-						where invi_int_id = ID_ITEM; 	
-	
-		END CASE;
+        
+        -- cantidad x Pedido        
+        select pedd_int_cantidad 
+        into CANTIDAD_PEDIDO
+        from tb_pedidos_detalle where pro_int_id = id;
+        
+        BLOCK2: BEGIN
+        DECLARE curprodet CURSOR FOR SELECT invi_int_id FROM tb_productos_detalle where pro_int_id = id;
+        DECLARE CONTINUE HANDLER FOR NOT FOUND SET finished2 = 1;
+        OPEN curprodet; 
+        cur_attribute_loop: LOOP
+        FETCH FROM curprodet INTO id_item;   
+            IF finished2 = 1 THEN
+				 LEAVE cur_attribute_loop;	
+			END IF; 
+            
+			-- caractersticas del producto detalle
+			select prod_int_tipo_medida_salida , prod_dbl_cantidad_item , invi_int_id 
+			into PRO_UNIDADMEDIDA, VALOR_PRODUCTO_DETALLE, ID_ITEM 
+			from tb_productos_detalle where pro_int_id = id and invi_int_id = id_item;
+			
+			-- caractersticas del ITEM
+			select invi_dbl_cantidad_total_item , invi_dbl_peso_neto , itm_int_tipo_medida_entrada 
+			into CANTIDAD_ITEM, PESO_ITEM, ITEM_UMEDIDA from tb_item_inventario
+			where invi_int_id = ID_ITEM and invi_int_id = id_item;      
+
+			-- VALIDACIONES DE ITEM
+			IF(PESO_ITEM > 0 AND CANTIDAD_ITEM > 0)	THEN
+				UPDATE tb_item_inventario set 
+				invi_dbl_cantidad_total_item = CANTIDAD_ITEM - (VALOR_PRODUCTO_DETALLE * CANTIDAD_PEDIDO),
+				invi_dbl_peso_neto = PESO_ITEM - (VALOR_PRODUCTO_DETALLE * CANTIDAD_PEDIDO)
+				where invi_int_id = ID_ITEM;
+				SELECT id;
+			END IF;     
+            
+            CLOSE curprodet;
+        END LOOP cur_attribute_loop;
+        END BLOCK2;
+            
 	END LOOP getIdProducto;
 	CLOSE curProductoPedido;
+    SELECT id;
 END //
 DELIMITER ;
 
   /* CALL SP_PEDIDO_VENTA () */
+
 
